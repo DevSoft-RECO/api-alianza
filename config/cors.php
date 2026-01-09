e7ce13d47c75aa5184e74a1bf324ec298830e632
DevSoft-RECO <ronycardonaordonez@gmail.com>
2026-01-08 23:20:01 -0600

    agregando ruta web para no cambiar

--- config/cors.php
+++ config/cors.php
@@ -36,15 +36,19 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    
+
     // AQUÍ EL CAMBIO CLAVE:
-    'allowed_origins' => ['http://localhost:5173'], // Tu URL de servidor ya en produccion
-    
+     'allowed_origins' => [
+        'https://alianzaepaecciledna.edu.gt',       // Tu dominio principal (Producción)
+        'https://www.alianzaepaecciledna.edu.gt',   // Es bueno agregar la versión www también
+        'http://localhost:5173',                    // Puedes dejarlo si a veces conectas tu PC local a este server
+    ],
+
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    
+
     // Y ESTE TAMBIÉN EN TRUE:
     'supports_credentials' => false,
 
